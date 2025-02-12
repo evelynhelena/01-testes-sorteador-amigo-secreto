85d31de10e2ee5dc40b430f736614b8f30323e79
evelynhelena <evelynhelena781@gmail.com>
2025-02-11 21:16:47 -0300

    criando formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react"
 import { useAddParticipant } from "../../hook/useAddParticipant";
 import { useMessageError } from "../../hook/useMessageError";
 
-export function Form(){
+export function Form() {
     const [particpantName, setParticpantName] = useState<string>('');
     const inputRef = useRef<HTMLInputElement>(null);
     const addParticipantList = useAddParticipant();
@@ -16,16 +16,22 @@ export function Form(){
     }
 
     return (
-        <div>
-            <form onSubmit={addParticipants}>
+        <div className="w-full flex justify-center flex-col items-center gap-8">
+            <p className="text-2xl">Vamos começar!</p>
+            <form onSubmit={addParticipants} className="w-full max-w-[500px] flex">
                 <input
-                ref={inputRef} 
-                placeholder="Insira os nomes dos participantes" 
-                value={particpantName}
-                type="text" 
-                onChange={({target}) => setParticpantName(target.value)}/>
-                <button aria-label="button-adding" disabled={!particpantName}>Adicionar</button>
-               {errorMessage && <p role='alert'>{errorMessage}</p>} 
+                    className="border-b-5 border-2 w-full p-[17px] rounded-l-[37px]"
+                    ref={inputRef}
+                    placeholder="Insira os nomes dos participantes"
+                    value={particpantName}
+                    type="text"
+                    onChange={({ target }) => setParticpantName(target.value)} />
+                <button
+                    className={`${!particpantName && 'cursor-not-allowed'} p-[17px] rounded-r-[37px] border-b-5 border-2 w-[255px] bg-color-gray`}
+                    aria-label="button-adding"
+                    disabled={!particpantName}>Adicionar
+                </button>
+                {errorMessage && <p role='alert'>{errorMessage}</p>}
             </form>
         </div>
     )
